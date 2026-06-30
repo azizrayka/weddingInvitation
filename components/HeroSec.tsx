8ecae5eb8b830f7ba25805dc272bbb99c141484a
mohamedaziz <oualimohamedaziz89@gmail.com>
2026-07-01 00:02:09 +0100

    Describe what you changed

--- components/HeroSec.tsx
+++ components/HeroSec.tsx
@@ -36,20 +36,18 @@ const HeroSec = ({state}:{state:boolean}) => {
   return (
     <div className="hero" ref={component}>
       <div className="relative h-screen w-screen text-white overflow-hidden bg-[url(/am.jpg)] bg-cover bg-center flex justify-center items-center">
-        <div className="flex flex-col items-center mt-10">
+        <div className="flex flex-col items-center">
             <Image
               src="/names.png"
               alt="OF"
               width={400}
               height={70}
-              className="absolute object-contain top-75 w-auto h-auto names"
+              className="object-contain top-75 w-auto h-auto names"
               style={{ opacity: 0 }}
               priority
             />
-        </div>
-
-        {/* Nzoul Box */}
-        <div className="absolute px-5 backdrop-blur-sm p-3 rounded-lg top-[58%] left-0 right-0 mx-auto w-fit flex flex-row gap-5 items-center text-black nzoul" style={{ opacity: 0 }}>
+            {/* Nzoul Box */}
+        <div className="px-5 backdrop-blur-sm p-3 rounded-lg top-[58%] left-0 right-0 mx-auto w-fit flex flex-row gap-5 items-center text-black nzoul" style={{ opacity: 0 }}>
           <span className="w-20 text-center">Nzoul</span>
           <div className="h-[80px] w-1 border-b-[3px] border-black bg-black"></div>
           <span className="flex flex-col items-center">
@@ -73,6 +71,9 @@ const HeroSec = ({state}:{state:boolean}) => {
           <div className="h-[80px] w-1 border-b-[3px] border-black bg-black"></div>
           <span className="w-20 text-center">At 9:00 PM</span>
         </div>
+        </div>
+
+        
 
       </div>
       <Detailes />
